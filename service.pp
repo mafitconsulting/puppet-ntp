dba29345920951075c4ce1089210498857770bb4
root <root@fieldhousem2.mylabserver.com>
2017-04-03 15:28:20 +0000

    First commit

---
+++ service.pp
@@ -0,0 +1,17 @@
+class ntp::service (
+  String $service_name        = $ntp::service_name,
+  String $service_ensure      = $ntp::service_ensure,
+  Boolean $service_enabled    = $ntp::service_enabled,
+  Boolean $service_hasstatus  = $ntp::service_hasstatus,
+  Boolean $service_hasrestart = $ntp::service_hasrestart
+) {
+  service { 'ntp': 
+    ensure     => $service_ensure,
+    enable     => $service_enable,
+    name       => $service_name,
+    hasstatus  => $service_hasstatus,
+    hasrestart => $service_hasrestart,
+  }
+}
+   
+
